59c7981dc5b760e67561ba2e1e16e22a1ba2e484
Moajjem Hossain <moajjem441@gmail.com>
2026-08-19 00:13:04 +0600

    air drawing part updated for usb connection code

--- air_drawing.py
+++ air_drawing.py
@@ -6,6 +6,7 @@ import numpy as np
 # Mobile camera URL (my IP)
 # cap = cv2.VideoCapture("http://192.168.0.106:4747/video")
 
+#for usb connection with droid and mobile
 cap = cv2.VideoCapture(0)
 
 
